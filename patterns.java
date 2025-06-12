dbb0868e0aa9b0849eedd0e86e441100c0d8266c
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-06-12 10:06:45 +0530

    Update patterns.java

--- patterns.java
+++ patterns.java
@@ -1,1 +1,23 @@
-
+import java.util.*;
+public class problem1{
+    public static void main(String[] args) {
+    for(int i=0; i<5; i++){
+        for(int j=0; j<5; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    for(int i=0; i<5; i++){
+        for(int j=0; j<i+1; j++){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    for(int i=4; i>=0; i--){
+        for(int j=i; j>=0; j--){
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+  }
+}
