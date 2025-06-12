9f1a7afebfcf8995b92d4966d29a49a6c38a648e
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-06-12 10:02:36 +0530

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -1,3 +1,21 @@
+/*A supermarket wants to build a system to calculate the total bill for each customer who buys multiple items. The number of items a customer buys is unknown in advance.
+The system should:
+1.Ask the customer for the number of items they wish to buy.
+2.For each item, ask for the item price.
+3.Use aloop (choose the appropriate type) to read all item prices.
+4.Finally, display the total amount to be paid by the customer.
+Use proper loop logic to handle the dynamic number of items.
+Note :
+ - Use a for loop if the number of items is fixed at the start.
+Sample Input :
+Enter number of items: 4  
+Enter price of item 1: 50  
+Enter price of item 2: 30  
+Enter price of item 3: 20  
+Enter price of item 4: 100  
+Sample Output :
+Total bill: 200*/
+
 import java.util.*;
 public class Main{
     public static void main(String[] args) {
