7bd338d07dead8c2611ad21d7ae7103bdee4afb6
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-06-18 20:50:02 +0530

    Add files via upload

---
+++ Problem8.java
@@ -0,0 +1,24 @@
+package placement;
+import java.util.*;
+public class Problem8{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter no. of arr elements: ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			System.out.println("Enter the array element :");
+			arr[i] = sc.nextInt();
+		}
+		Set<Integer> set = new LinkedHashSet<>();
+		for(int num: arr) {
+			set.add(num);
+		}
+		int i=0;
+		int[] res = new int[set.size()];
+		for(int num: set) {
+			res[i++] = num;
+		}
+		System.out.println("Arr : " + Arrays.toString(res));
+	}
+}
