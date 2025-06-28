700cb6e39c7e2c8996fef35845f22651039afd63
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-06-28 20:48:14 +0530

    Update problem13.java

--- problem13.java
+++ problem13.java
@@ -1,4 +1,15 @@
-package placement;
+/*You are required to implement the following function:
+Int Calculate(int m, int n);
+The function accepts 2 positive integer ‘m’ and ‘n’ as its arguments.You are required to calculate the sum of numbers divisible both by 3 and 5, between ‘m’ and ‘n’ both inclusive and return the same.
+Note
+0 < m <= n
+Test Case 
+Input:
+m : 12
+n : 50
+Output
+90*/
+
 import java.util.*;
 public class problem13 {
 	public static int Cal(int m, int n) {
@@ -17,5 +28,6 @@ public class problem13 {
         System.out.println("enter n value: ");
         int n = sc.nextInt();
         System.out.println(Cal(m, n));
+	sc.close();
     }
 }
