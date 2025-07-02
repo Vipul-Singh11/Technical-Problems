ebcb2e40ec4c187f7ed5e3f323bf3267195e90a0
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-07-02 21:07:29 +0530

    Update problem15.java

--- problem15.java
+++ problem15.java
@@ -1,4 +1,11 @@
-package placement;
+/*Rearrange the array such that the first half is arranged in increasing order, and the second half is arranged in decreasing order
+Test Case 1:
+Input: 8 7 1 6 5 9
+Output: 1 5 6 9 8 7
+Test Case 2:
+Input: 4 2 8 6 15 5 9 20
+Output: 2 4 5 6 20 15 9 8*/
+
 import java.util.*;
 public class problem15 {
     public static void main(String[] args) {
