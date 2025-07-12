9c92a1f0da5435278b499843abb4c45d0dbeb2a6
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-07-12 19:10:34 +0530

    Update problem21.java

--- problem21.java
+++ problem21.java
@@ -1,27 +1,29 @@
-package placement;
-import java.util.Arrays;
+/*Find the First Missing Positive Integer
+Problem: Find the smallest positive integer missing from the array.
+Input: [3, 4, -1, 1]
+Output: 2*/
+
+import java.util.HashSet;
 import java.util.Scanner;
-public class problem21 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("enter length of array: ");
-		int n = sc.nextInt();
-		int[] arr = new int[n];
-		for(int i=0; i<n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		int k = 1;
-		for(int num : arr) {
-			if(k == num) {
-				k++;
-			}else if(k<num) {
-				System.out.println(k);
-				sc.close();
-				return;
-			}
-		}
-		System.out.println(k);
-		sc.close();
-	}
+public class problem21{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if (arr[i] > 0) {
+                set.add(arr[i]);
+            }
+        }
+        for (int i = 1; i <= n + 1; i++) {
+            if (!set.contains(i)) {
+                System.out.println(i);
+                break;
+            }
+        }
+        sc.close();
+    }
 }
