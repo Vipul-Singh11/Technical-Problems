c882ccb3fc3cefc233770914c159a8cc431e6ed3
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-07-16 19:29:52 +0530

    Update problem22.java

--- problem22.java
+++ problem22.java
@@ -1,4 +1,20 @@
-package placement;
+/*A chocolate factory produces premium chocolates with custom names printed on their wrappers. Due to a printing machine glitch, some letters are duplicated in the product name. As the quality assurance engineer, your job is to write a program that cleans up the product label by removing duplicate characters (while preserving the original character order).
+Write a Java program that reads a product name and returns a version of the name with duplicate characters removed.
+Input:
+A string productName – the name printed on the chocolate box label (may contain duplicate characters, case-sensitive).
+Output:
+A string with all duplicate characters removed, keeping only the first occurrence of each character.
+Test case 01 :
+Input :
+chococolate
+Output :
+cholate
+Test case 02 :
+Input :
+darkchocolate
+output :
+darkcholte*/
+
 import java.util.*;
 public class problem22{
 	public static void main(String[] args) {
