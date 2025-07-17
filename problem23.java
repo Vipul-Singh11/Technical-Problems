6a5c1fa19b4e1ce31eaff33a35be031d38e8bad8
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-07-17 20:09:06 +0530

    Update problem23.java

--- problem23.java
+++ problem23.java
@@ -1,4 +1,9 @@
-package placement;
+/*You are planning a boat trip across a series of islands. The islands are arranged in a straight line and are numbered from 0 to N−1. Each island has a board that displays the maximum number of islands you can jump forward from there using your boat (based on the tide levels and wind).
+You start at island 0, and your goal is to reach the last island (island N−1) using the minimum number of jumps.
+Write a Java program to determine the minimum number of jumps required to reach the last island. If it's not possible, return -1.
+Input: [2, 3, 1, 1, 4]
+Output: 2*/
+
 import java.util.*;
 public class problem23 {
     public static void main(String[] args) {
