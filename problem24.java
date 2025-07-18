4614cc33a7400ea7d35cd7724fc06590905b52f6
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-07-18 20:07:18 +0530

    Add files via upload

---
+++ problem24.java
@@ -0,0 +1,33 @@
+package placement;
+import java.util.*;
+public class problem24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of elements: ");
+        int n = sc.nextInt();
+        if(n==0) {
+        	System.out.println(0);
+        	return;
+        }
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        if(n==1 && arr[0]==0) {
+        	System.out.println(1);
+        	return;
+        }
+        int maxlength = 0;
+        for(int i=0; i<n; i++) {
+        	int sum = 0;
+        	for(int j=i; j<n; j++) {
+        		sum += arr[j];
+        		if(sum == 0) {
+        			maxlength = Math.max(maxlength, j-i+1);
+        		}
+        	}
+        }
+        System.out.println(maxlength);
+        sc.close();
+    }
+}
