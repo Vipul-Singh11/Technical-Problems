e76666d52d912bc3ae8783f402b6c75351b545c0
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-07-25 07:45:42 +0530

    Update problem25.java

--- problem25.java
+++ problem25.java
@@ -1,4 +1,9 @@
-package placement;
+/*Given an array of integers arr[] where each element represents the maximum number of steps that can be made forward from that element. Write a program to return the minimum number of jumps to reach the end of the array (starting from the first element). If the end isn't reachable, return -1.
+Input :
+arr = {2, 3, 1, 1, 4}
+Output :
+Minimum Jumps = 2*/
+
 import java.util.*;
 public class problem25 {
     public static void main(String[] args) {
