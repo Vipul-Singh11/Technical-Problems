a9fb9ce976d968f61d8de473a4c33ccc41222b97
Vipul-Singh11 <itsvipulsingh11@gmail.com>
2025-07-30 19:14:34 +0530

    Add files via upload

---
+++ problem28.java
@@ -0,0 +1,31 @@
+package placement;
+import java.util.*;
+public class problem28 {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter input: ");
+    	int n = sc.nextInt();
+    	if(n==1) {
+    		System.out.println(1);
+    		sc.close();
+    		return;
+    	}
+    	String prev = "1";
+    	for(int i=2; i<=n; i++) {
+    		StringBuilder sb = new StringBuilder();
+    		int count = 1;
+    		for(int j=1; j<prev.length(); j++) {
+    			if(prev.charAt(j) == prev.charAt(j-1)) {
+    				count++;
+    			}else {
+    				sb.append(count).append(prev.charAt(j-1));
+    				count = 1;
+    			}
+    		}
+    		sb.append(count).append(prev.charAt(prev.length()-1));
+    		prev = sb.toString();
+    	}
+    	System.out.println(prev);
+    	sc.close();
+    }
+}
